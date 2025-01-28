a93d6a6a0edab7a7e442e3c77ac615a81dc92620
meodel <alf.meodel@gmail.com>
2025-01-28 12:19:18 +0100

    Clean up project structure

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.enableCors();
+  await app.listen(9036, '0.0.0.0');
 }
 bootstrap();
